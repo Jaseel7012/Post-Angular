308f6c2d7d1c974b8df31bb3a94fbe91e860141a
jaseel <jaseeljctcs@gmail.com>
2022-11-28 14:15:28 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostAppAddComponent } from './post-app-add/post-app-add.component';
+import { PostViewComponent } from './post-view/post-view.component';
+const myroute:any=[
+  {
+    path:'',
+    'component':PostAppAddComponent
+  },
+  {
+    path:'view',
+    'component':PostViewComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostAppAddComponent,
+    PostViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post-app-add/post-app-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-app-add',
+  templateUrl: './post-app-add.component.html',
+  styleUrls: ['./post-app-add.component.css']
+})
+export class PostAppAddComponent {
+
+}
